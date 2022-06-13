88a04331877e550cb4503e177a96ed18db082d4d
codestickdev <piotrdevv@gmail.com>
2022-06-13 22:37:44 +0200

    Realization sections

--- js/_sliders.js
+++ js/_sliders.js
@@ -9,5 +9,36 @@
             arrows: false,
             dots: true,
         });
+
+        // Realizations
+        $('.homeRealizations__content').each(function(){
+            let content = $(this).find('.content'),
+                images = $(this).find('.images');
+
+            $(content).slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: false,
+                asNavFor: $(images),
+                fade: true,
+                responsive: [
+                    {
+                        breakpoint: 991,
+                        settings: {
+                            fade: false,
+                            dots: true,
+                        }
+                    }
+                ]
+            });
+            $(images).slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                dots: false,
+                asNavFor: $(content),
+            });
+        });
     });
 }(jQuery));
--- page_templates/page_home.php
+++ page_templates/page_home.php
@@ -70,7 +70,169 @@ get_header(); ?>
                 </div>
             </div>
         </section>
+        <section class="homeInfo">
+            <div class="homeInfo__wrap container">
+                <div class="homeInfo__heading">
+                    <h2><span>Mill-yon</span>w liczbach</h2>
+                </div>
+                <div class="homeInfo__list">
+                    <div class="box">
+                        <div class="box__icon">
+                            <img src="<?php echo get_template_directory_uri() . '/images/homeInfo_01.svg'; ?>"/>
+                        </div>
+                        <h3>16 lat</h3>
+                        <p>na polskim rynku</p>
+                    </div>
+                    <div class="box">
+                        <div class="box__icon">
+                            <img src="<?php echo get_template_directory_uri() . '/images/homeInfo_02.svg'; ?>"/>
+                        </div>
+                        <h3>3967</h3>
+                        <p>sprzedanych mieszkań</p>
+                    </div>
+                    <div class="box">
+                        <div class="box__icon">
+                            <img src="<?php echo get_template_directory_uri() . '/images/homeInfo_03.svg'; ?>"/>
+                        </div>
+                        <h3>21</h3>
+                        <p>zrealizowanych inwestycji</p>
+                    </div>
+                    <div class="box">
+                        <div class="box__icon">
+                            <img src="<?php echo get_template_directory_uri() . '/images/homeInfo_04.svg'; ?>"/>
+                        </div>
+                        <h3>239 000 m²</h3>
+                        <p>powierzchni inwestycji</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
+    <section class="homeRealizations">
+        <div class="homeRealizations__wrap container">
+            <div class="homeRealizations__heading">
+                <h2>W sprzedaży</h2>
+            </div>
+            <div class="homeRealizations__content">
+                <div class="content">
+                    <div class="content__slide">
+                        <h3 class="name">Aura40</h3>
+                        <h4 class="localization">Gdańsk | Śródmieście</h4>
+                        <div class="desc">
+                            <p>Lokalizacja tuż przy historycznym centrum miasta w cichej, zielonej okolicy, zapewnia fantastyczne warunki niezależnie od tego czy chcesz tu zamieszkać z rodziną czy zainwestować w apartament na wynajem.</p>
+                        </div>
+                        <div class="actions">
+                            <a href="#" class="btn"><span>Strona inwestycji</span></a>
+                            <a href="#" class="btn video"><span>Zobacz film</span></a>
+                        </div>
+                    </div>
+                    <div class="content__slide">
+                        <h3 class="name">Aura40</h3>
+                        <h4 class="localization">Gdańsk | Śródmieście</h4>
+                        <div class="desc">
+                            <p>Lokalizacja tuż przy historycznym centrum miasta w cichej, zielonej okolicy, zapewnia fantastyczne warunki niezależnie od tego czy chcesz tu zamieszkać z rodziną czy zainwestować w apartament na wynajem.</p>
+                        </div>
+                        <div class="actions">
+                            <a href="#" class="btn"><span>Strona inwestycji</span></a>
+                            <a href="#" class="btn video"><span>Zobacz film</span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="images">
+                    <div class="images__image">
+                        <img src="<?php echo get_template_directory_uri() . '/images/realization_01.png'; ?>"/>
+                    </div>
+                    <div class="images__image">
+                        <img src="<?php echo get_template_directory_uri() . '/images/realization_01.png'; ?>"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="homeRealizations homeRealizations--white">
+        <div class="homeRealizations__wrap container">
+            <div class="homeRealizations__heading">
+                <h2>W przygotowaniu</h2>
+            </div>
+            <div class="homeRealizations__content">
+                <div class="content">
+                    <div class="content__slide">
+                        <h3 class="name">Aura Nova</h3>
+                        <h4 class="localization">Ożarów Mazowiecki</h4>
+                        <div class="desc">
+                            <p>Aura Nova to wyjątkowa inwestycja zlokalizowana w doskonale skomunikowanej z Warszawą, spokojnej części Ożarowa Mazowieckiego. Znajdziesz tutaj wszystko, czego potrzebujesz do wygodnego życia.</p>
+                            <p>Kameralna, niska zabudowa, funkcjonalne mieszkania, bliskość usług, terenów zielonych oraz rekreacyjnych, a także wygodny dostęp do stolicy – to wszystko masz na wyciągnięcie ręki. </p>
+                        </div>
+                        <div class="actions">
+                            <a href="#" class="btn"><span>Strona inwestycji</span></a>
+                            <a href="#" class="btn video"><span>Zobacz film</span></a>
+                        </div>
+                    </div>
+                    <div class="content__slide">
+                        <h3 class="name">Aura Nova</h3>
+                        <h4 class="localization">Ożarów Mazowiecki</h4>
+                        <div class="desc">
+                            <p>Aura Nova to wyjątkowa inwestycja zlokalizowana w doskonale skomunikowanej z Warszawą, spokojnej części Ożarowa Mazowieckiego. Znajdziesz tutaj wszystko, czego potrzebujesz do wygodnego życia.</p>
+                            <p>Kameralna, niska zabudowa, funkcjonalne mieszkania, bliskość usług, terenów zielonych oraz rekreacyjnych, a także wygodny dostęp do stolicy – to wszystko masz na wyciągnięcie ręki. </p>
+                        </div>
+                        <div class="actions">
+                            <a href="#" class="btn"><span>Strona inwestycji</span></a>
+                            <a href="#" class="btn video"><span>Zobacz film</span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="images">
+                    <div class="images__image">
+                        <img src="<?php echo get_template_directory_uri() . '/images/realization_01.png'; ?>"/>
+                    </div>
+                    <div class="images__image">
+                        <img src="<?php echo get_template_directory_uri() . '/images/realization_01.png'; ?>"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="homeRealizations">
+        <div class="homeRealizations__wrap container">
+            <div class="homeRealizations__heading">
+                <h2>Zakończone</h2>
+            </div>
+            <div class="homeRealizations__content">
+                <div class="content">
+                    <div class="content__slide">
+                        <h3 class="name">Aura Park</h3>
+                        <h4 class="localization">Warszawa | Wilanów</h4>
+                        <div class="desc">
+                            <p>Lokalizacja tuż przy historycznym centrum miasta w cichej, zielonej okolicy, zapewnia fantastyczne warunki niezależnie od tego czy chcesz tu zamieszkać z rodziną czy zainwestować w apartament na wynajem.</p>
+                        </div>
+                        <div class="actions">
+                            <a href="#" class="btn"><span>Strona inwestycji</span></a>
+                            <a href="#" class="btn video"><span>Zobacz film</span></a>
+                        </div>
+                    </div>
+                    <div class="content__slide">
+                        <h3 class="name">Aura Park</h3>
+                        <h4 class="localization">Warszawa | Wilanów</h4>
+                        <div class="desc">
+                            <p>Lokalizacja tuż przy historycznym centrum miasta w cichej, zielonej okolicy, zapewnia fantastyczne warunki niezależnie od tego czy chcesz tu zamieszkać z rodziną czy zainwestować w apartament na wynajem.</p>
+                        </div>
+                        <div class="actions">
+                            <a href="#" class="btn"><span>Strona inwestycji</span></a>
+                            <a href="#" class="btn video"><span>Zobacz film</span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="images">
+                    <div class="images__image">
+                        <img src="<?php echo get_template_directory_uri() . '/images/realization_03.png'; ?>"/>
+                    </div>
+                    <div class="images__image">
+                        <img src="<?php echo get_template_directory_uri() . '/images/realization_03.png'; ?>"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php get_footer(); ?>
